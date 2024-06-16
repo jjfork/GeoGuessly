7cc8d6c2e440199592a7d137cf6b29d033e60787
j_4k <jakubiakrjulian@gmail.com>
2024-06-17 00:23:45 +0200

    working application, vaultkey in next commits

--- src/main/java/pwr/git/geoguessly/GeoguesslyApplication.java
+++ src/main/java/pwr/git/geoguessly/GeoguesslyApplication.java
@@ -1,25 +1,14 @@
 package pwr.git.geoguessly;
 
-import com.azure.security.keyvault.secrets.SecretClient;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeoguesslyApplication implements CommandLineRunner {
+public class GeoguesslyApplication  {
 
-	private final SecretClient secretClient;
-
-    public GeoguesslyApplication(SecretClient secretClient) {
-        this.secretClient = secretClient;
-    }
 
     public static void main(String[] args) {
 		SpringApplication.run(GeoguesslyApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
 }

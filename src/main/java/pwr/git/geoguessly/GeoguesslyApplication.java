b9cd78c9baef62c33aa2135865e69604111dc415
j_4k <jakubiakrjulian@gmail.com>
2024-06-16 22:57:21 +0200

    added new functionality, added azure key vault

--- src/main/java/pwr/git/geoguessly/GeoguesslyApplication.java
+++ src/main/java/pwr/git/geoguessly/GeoguesslyApplication.java
@@ -1,13 +1,25 @@
 package pwr.git.geoguessly;
 
+import com.azure.security.keyvault.secrets.SecretClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeoguesslyApplication {
+public class GeoguesslyApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	private final SecretClient secretClient;
+
+    public GeoguesslyApplication(SecretClient secretClient) {
+        this.secretClient = secretClient;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(GeoguesslyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }

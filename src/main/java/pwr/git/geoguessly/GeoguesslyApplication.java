b5d4a1527d2185d7c72f851a9187bc498a13fb64
j_4k <91293626+jjfork@users.noreply.github.com>
2024-06-18 22:48:48 +0200

    Production (#4)
    refactor

--- src/main/java/pwr/git/geoguessly/GeoguesslyApplication.java
+++ src/main/java/pwr/git/geoguessly/GeoguesslyApplication.java
@@ -1,14 +1,36 @@
 package pwr.git.geoguessly;
 
+import com.azure.security.keyvault.secrets.SecretClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GeoguesslyApplication  {
+public class GeoguesslyApplication implements CommandLineRunner {
 
+	private final SecretClient secretClient;
 
-    public static void main(String[] args) {
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
+
+	@Value("${spring.datasource.username}")
+	private String datasourceUsername;
+
+	@Value("${spring.datasource.password}")
+	private String datasourcePassword;
+
+	public GeoguesslyApplication(SecretClient secretClient) {
+		this.secretClient = secretClient;
+	}
+
+	public static void main(String[] args) {
 		SpringApplication.run(GeoguesslyApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Datasource URL: " + datasourceUrl);
+		System.out.println("Datasource Username: " + datasourceUsername);
+	}
 }
